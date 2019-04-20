a4e4a8f8e02563b89f822e8de18660c5bed6775b
unknown <NJSobral>
2019-04-20 11:08:22 +0100

    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 8975258,
+    "boundingBox": {
+        "lx": 554033.5056211499,
+        "ly": 4505756.418884926,
+        "lz": -38.954028820046378,
+        "ux": 554456.6114941492,
+        "uy": 4506179.5247579249,
+        "uz": 384.1518441792483
+    },
+    "tightBoundingBox": {
+        "lx": 554033.51,
+        "ly": 4505756.42,
+        "lz": -38.95,
+        "ux": 554456.61,
+        "uy": 4506098.76,
+        "uz": 66.57000000000001
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.931363582611084,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
